bebdae9f4e5e6b69a2363d23cedf968c3740843f
RmK9 <th3rmk@gmail.com>
2016-03-14 02:54:50 +0000

    initial commit

---
+++ CM3036 Coursework - Kolesov1308140/InputValidator.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CM3036_Coursework___Kolesov1308140
+{
+    public static class InputValidator
+    {
+        #region InputValidator - Allow only numbers input handling
+
+        public static void AllowOnlyNumbers_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            //Get Key from event, cast it to string and then check if it's not a digit with regex
+            //AND if pressed Key is not a backspace
+            if (Regex.IsMatch(KeyToCharCaster.GetCharFromKey(e.Key).ToString(), @"[^0-9]") &&
+                e.Key != Key.Back && !IsModifierOrArrowKeyDown(e))
+            {
+                //Prevent input of forbidden character
+                e.Handled = true;
+            }
+
+        }
+
+        public static void AllowOnlyNumbers_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = e.Source as TextBox;
+            if (textBox != null && Regex.IsMatch(textBox.Text, @"[^0-9]"))
+            {
+                e.Handled = true;
+            }
+        }
+
+        public static void AllowOnlyNumbers_PasteHandler(object sender, DataObjectPastingEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+            bool allow = false;
+
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                string pasteText = e.DataObject.GetData(typeof(string)) as string;
+                if (pasteText != null && Regex.IsMatch(pasteText, @"[0-9]"))
+                {
+                    allow = true;
+                }
+            }
+
+            if (!allow)
+            {
+                e.CancelCommand();
+            }
+
+        }
+
+        #endregion
+
+        #region InputValidator - Allow only Alphabetical Characters input handling
+
+        public static void AllowOnlyAlphabeticalChars_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            //Get Key from event, cast it to string and then check if it's not a digit with regex
+            //AND if pressed Key is not a backspace
+            if (Regex.IsMatch(KeyToCharCaster.GetCharFromKey(e.Key).ToString(), @"^[^a-z^A-Z^'^\.]+$") &&
+                e.Key != Key.Back && !IsModifierOrArrowKeyDown(e))
+            {
+                //Prevent input of forbidden character
+                e.Handled = true;
+            }
+
+        }
+
+        public static void AllowOnlyAlphabeticalChars_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = e.Source as TextBox;
+            if (textBox != null && Regex.IsMatch(textBox.Text, @"^[^a-z^A-Z^'^\.]+$"))
+            {
+                e.Handled = true;
+            }
+        }
+
+        public static void AllowOnlyAlphabeticalChars_PasteHandler(object sender, DataObjectPastingEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+            bool allow = false;
+
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                string pasteText = e.DataObject.GetData(typeof(string)) as string;
+                if (pasteText != null && Regex.IsMatch(pasteText, @"^[a-zA-Z'\.]+$"))
+                {
+                    allow = true;
+                }
+            }
+
+            if (!allow)
+            {
+                e.CancelCommand();
+            }
+
+        }
+
+        #endregion
+
+        #region InputValidator - Allow only Grade Letters input handling
+
+        public static void AllowOnlyGradeLetters_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            //Get Key from event, cast it to string and then check if it's not a digit with regex
+            //AND if pressed Key is not a backspace
+            if (Regex.IsMatch(KeyToCharCaster.GetCharFromKey(e.Key).ToString(), @"^[^ABCDEFabcdef]+$") &&
+                e.Key != Key.Back && !IsModifierOrArrowKeyDown(e))
+            {
+                //Prevent input of forbidden character
+                e.Handled = true;
+            }
+
+        }
+
+        public static void AllowOnlyGradeLetters_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = e.Source as TextBox;
+            if (textBox != null && Regex.IsMatch(textBox.Text, @"^[^ABCDEFabcdef]+$"))
+            {
+                e.Handled = true;
+            }
+        }
+
+        public static void AllowOnlyGradeLetters_PasteHandler(object sender, DataObjectPastingEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+            bool allow = false;
+
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                string pasteText = e.DataObject.GetData(typeof(string)) as string;
+                if (pasteText != null && Regex.IsMatch(pasteText, @"^[ABCDEFabcdef]+$"))
+                {
+                    allow = true;
+                }
+            }
+
+            if (!allow)
+            {
+                e.CancelCommand();
+            }
+
+        }
+
+        #endregion
+
+        public static bool IsModifierOrArrowKeyDown(KeyEventArgs e)
+        {
+            return ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control) ||
+                   ((Keyboard.Modifiers & ModifierKeys.Alt) == ModifierKeys.Alt) ||
+                   (e.Key == Key.Left) || (e.Key == Key.Right) || (e.Key == Key.Up) || (e.Key == Key.Down);
+        }
+
+    }
+
+}
---
+++ CM3036 Coursework - Kolesov1308140/MainWindow.xaml.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace CM3036_Coursework___Kolesov1308140
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly StudentEntities _studentEntities = new StudentEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            BindListBoxData();
+
+            AttachTextBoxInputEventHandlers();
+        }
+
+        public List<Student> GetStudentList()
+        {
+            try
+            {
+                //Force open the connection
+                _studentEntities.Database.Connection.Open();
+                
+                //Gets the student list from DB in a reverse order to keep the records
+                //in the same order as in DB and keep oldest records at top
+                return _studentEntities.Students.AsEnumerable().Reverse().ToList();
+            }
+            catch (EntityException ex)
+            {
+                Console.WriteLine(ex.Message);
+                MessageBox.Show("Error while trying to get Students from Database\nError Message: " + ex.Message +
+                                "\nPlease try to run the application again.");
+                return new List<Student>();
+            }
+            catch (TimeoutException ex)
+            {
+                Console.WriteLine(ex.Message);
+                MessageBox.Show("Error while trying to get Students from Database\nError Message: " + ex.Message +
+                                "\nPlease try to run the application again.");
+                return new List<Student>();
+            }
+            finally
+            {
+                //Close the connection
+                _studentEntities.Database.Connection.Close();
+            }
+        }
+
+        public void SaveStudentChanges(Student changedStudent, int matricNumber)
+        {
+            try
+            {
+                //Force open the connection
+                _studentEntities.Database.Connection.Open();
+                //_studentEntities.Students.wh
+            }
+            catch (EntityException ex)
+            {
+                Console.WriteLine(ex.Message);
+                MessageBox.Show("Error while trying to get Students from Database\nError Message: " + ex.Message +
+                                "\nPlease try to run the application again.");
+            }
+            catch (TimeoutException ex)
+            {
+                Console.WriteLine(ex.Message);
+                MessageBox.Show("Error while trying to get Students from Database\nError Message: " + ex.Message +
+                                "\nPlease try to run the application again.");
+            }
+            finally
+            {
+                //Close the connection
+                _studentEntities.Database.Connection.Close();
+            }
+        }
+
+        public void BindListBoxData()
+        {
+            //Gets student list
+            StudentsListBox.ItemsSource = GetStudentList();
+        }
+
+        private string CalculateFinalGrade(Student student)
+        {
+            var finalGrade = "";
+
+
+
+            return finalGrade;
+        }
+
+        private Student GetSelectedStudent()
+        {
+            return StudentsListBox.SelectedItem as Student;
+        }
+
+        private void AttachTextBoxInputEventHandlers()
+        {
+            foreach (TextBox tb in FindLogicalChildren<TextBox>(this))
+            {
+                var textBoxType = tb.Name.Substring(tb.Name.Length - 1);
+                switch (textBoxType)
+                {
+                    case "N" :
+                        tb.PreviewKeyDown += InputValidator.AllowOnlyNumbers_OnKeyDown;
+                        tb.TextChanged += InputValidator.AllowOnlyNumbers_TextChanged;
+                        DataObject.AddPastingHandler(tb, InputValidator.AllowOnlyNumbers_PasteHandler);
+                        break;
+                    case "S" :
+                        tb.PreviewKeyDown += InputValidator.AllowOnlyAlphabeticalChars_OnKeyDown;
+                        tb.TextChanged += InputValidator.AllowOnlyAlphabeticalChars_TextChanged;
+                        DataObject.AddPastingHandler(tb, InputValidator.AllowOnlyAlphabeticalChars_PasteHandler);
+                        break;
+                    case "G" :
+                        tb.PreviewKeyDown += InputValidator.AllowOnlyGradeLetters_OnKeyDown;
+                        tb.TextChanged += InputValidator.AllowOnlyGradeLetters_TextChanged;
+                        DataObject.AddPastingHandler(tb, InputValidator.AllowOnlyGradeLetters_PasteHandler);
+                        break;
+                }
+            }
+        }
+
+        private void ApplyChangesButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var selectedStudent = GetSelectedStudent();
+
+            //Early exit (do nothing) if no student selected
+            if (selectedStudent == null) return;
+
+            _studentEntities.Students.Attach(selectedStudent);
+
+            selectedStudent.matriculationNumber = int.Parse(MatriculationNumberTextBoxN.Text);
+            selectedStudent.firstName = FirstNameTextBoxS.Text;
+            selectedStudent.lastName = LastNameTextBoxS.Text;
+            selectedStudent.componentOne = ComponentOneTextBoxG.Text;
+            selectedStudent.componentTwo = ComponentTwoTextBoxG.Text;
+            selectedStudent.componentThree = ComponentThreeTextBoxG.Text;
+
+            _studentEntities.Entry(selectedStudent).State = EntityState.Modified;
+            _studentEntities.SaveChanges();
+
+            //Refresh ListBox data
+            BindListBoxData();
+        }
+
+        public static IEnumerable<T> FindLogicalChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj == null) yield break;
+
+            foreach (DependencyObject child in LogicalTreeHelper.GetChildren(depObj).OfType<DependencyObject>())
+            {
+                if (child is T)
+                {
+                    yield return (T)child;
+                }
+
+                foreach (T childOfChild in FindLogicalChildren<T>(child))
+                {
+                    yield return childOfChild;
+                }
+            }
+        }
+
+        private void AddNewStudentButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var popup = new Window();
+            popup.ShowDialog();
+        }
+    }
+}
